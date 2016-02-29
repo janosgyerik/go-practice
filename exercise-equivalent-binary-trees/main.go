b5611da6dd287be804cfc8740d7e5d8b476543ca
Janos Gyerik <janos@kronos>
2016-02-29 20:51:06 +0100

    exercise-equivalent-binary-trees/main.go

---
+++ exercise-equivalent-binary-trees/main.go
@@ -0,0 +1,41 @@
+package main
+
+import (
+	"fmt"
+	"golang.org/x/tour/tree"
+)
+
+func Walk(t *tree.Tree, ch chan int) {
+	if t == nil {
+		return
+	}
+	ch <- t.Value
+	Walk(t.Left, ch)
+	Walk(t.Right, ch)
+}
+
+func Same(t1, t2 *tree.Tree) bool {
+	ch1 := make(chan int)
+	ch2 := make(chan int)
+	go Walk(t1, ch1)
+	go Walk(t2, ch2)
+
+	for i := 0; i < 5; i++ {
+		v1, ok1 := <-ch1
+		v2, ok2 := <-ch2
+		if ok1 != ok2 || v1 != v2 {
+			return false
+		}
+		if !ok1 {
+			break
+		}
+	}
+	return true
+}
+
+func main() {
+	t1 := tree.New(1)
+	t2 := tree.New(2)
+	fmt.Println(Same(t1, t1))
+	fmt.Println(Same(t1, t2))
+}
