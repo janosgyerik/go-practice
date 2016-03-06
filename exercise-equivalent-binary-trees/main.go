3cf76cc9709c08f362146fb6e01975b017f40cfa
Janos Gyerik <janos@kronos>
2016-03-06 21:14:42 +0100

    can eliminate iteration limit by closing channels after walk

--- exercise-equivalent-binary-trees/main.go
+++ exercise-equivalent-binary-trees/main.go
@@ -14,12 +14,18 @@ func Walk(t *tree.Tree, ch chan int) {
 }
 
 func Same(t1, t2 *tree.Tree) bool {
-	ch1 := make(chan int)
-	ch2 := make(chan int)
-	go Walk(t1, ch1)
-	go Walk(t2, ch2)
+	ch1 := make(chan int, 8)
+	ch2 := make(chan int, 8)
+	go func() {
+		Walk(t1, ch1)
+		close(ch1)
+	}()
+	go func() {
+		Walk(t2, ch2)
+		close(ch2)
+	}()
 
-	for i := 0; i < 5; i++ {
+	for {
 		v1, ok1 := <-ch1
 		v2, ok2 := <-ch2
 		if ok1 != ok2 || v1 != v2 {
