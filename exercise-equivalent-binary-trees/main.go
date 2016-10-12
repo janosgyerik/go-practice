6fb3876d7d0957a8398820516925c82e4937279c
Janos Gyerik <janos@kronos>
2016-10-12 22:44:36 +0200

    Improve equivalent binary trees impl

--- exercise-equivalent-binary-trees/main.go
+++ exercise-equivalent-binary-trees/main.go
@@ -6,16 +6,17 @@ import (
 )
 
 func Walk(t *tree.Tree, ch chan int) {
-	if t != nil {
-		ch <- t.Value
-		Walk(t.Left, ch)
-		Walk(t.Right, ch)
+	if t == nil {
+		return
 	}
+	Walk(t.Left, ch)
+	ch <- t.Value
+	Walk(t.Right, ch)
 }
 
 func Same(t1, t2 *tree.Tree) bool {
-	ch1 := make(chan int, 8)
-	ch2 := make(chan int, 8)
+	ch1 := make(chan int)
+	ch2 := make(chan int)
 	go func() {
 		Walk(t1, ch1)
 		close(ch1)
@@ -27,8 +28,7 @@ func Same(t1, t2 *tree.Tree) bool {
 
 	for {
 		v1, ok1 := <-ch1
-		v2, ok2 := <-ch2
-		if ok1 != ok2 || v1 != v2 {
+		if v2, ok2 := <-ch2; ok1 != ok2 || v1 != v2 {
 			return false
 		}
 		if !ok1 {
@@ -38,8 +38,6 @@ func Same(t1, t2 *tree.Tree) bool {
 }
 
 func main() {
-	t1 := tree.New(1)
-	t2 := tree.New(2)
-	fmt.Println(Same(t1, t1))
-	fmt.Println(Same(t1, t2))
+	fmt.Println(Same(tree.New(1), tree.New(1)) == true)
+	fmt.Println(Same(tree.New(1), tree.New(2)) == false)
 }
